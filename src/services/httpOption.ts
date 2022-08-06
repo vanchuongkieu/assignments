47aa01ba84700cf37984462f74eb1cdaacce71f0
vanchuongkieu <vanchuongkieu@gmail.com>
2022-08-06 22:51:01 +0700

    api prod

--- src/services/httpOption.ts
+++ src/services/httpOption.ts
@@ -1,14 +1,22 @@
 import axios from "axios";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 
+let apiUrl = "http://localhost:5000";
+
+if (process.env.NODE_ENV === "production") {
+  apiUrl = "https://chuong-nodejs-asm.vercel.app/";
+}
+
 export const http = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: apiUrl,
   timeout: 30000,
 });
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: apiUrl,
+  }),
   tagTypes: ["Categories", "Products", "Orders", "Users"],
   endpoints: () => ({}),
 });
